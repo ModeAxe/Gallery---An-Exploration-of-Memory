e197231259922d5e610d554cd3b7853c0b86d4ef
ModeAxe <modeaxe@gmail.com>
2020-02-12 10:22:49 -0500

    Curator Object Created

---
+++ Assets/Curator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Curator : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject[] artworks;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
