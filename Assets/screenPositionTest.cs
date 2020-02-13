52c662cfd996fe66abbaf9a630ba2b6b7545bb94
ModeAxe <modeaxe@gmail.com>
2020-02-12 23:50:00 -0500

    Started Navigation AI

---
+++ Assets/screenPositionTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screenPositionTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject target;
+    public Camera cam;
+    void Start()
+    {
+           
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 screenPosition = cam.WorldToScreenPoint(target.transform.position);
+        Debug.Log(screenPosition);
+    }
+}
