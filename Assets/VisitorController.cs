3c8fc4c4fba41046613a62c08a1c22593cc0c747
ModeAxe <modeaxe@gmail.com>
2020-02-20 23:47:23 -0500

    Started implementing collision and destination switching

--- Assets/VisitorController.cs
+++ Assets/VisitorController.cs
@@ -7,17 +7,33 @@ public class VisitorController : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject curator;
+    public bool arrived;
 
     public NavMeshAgent agent;
 
     void Start()
     {
+        arrived = false;
         agent.SetDestination(curator.GetComponent<Curator>().getArtwork());
     }
 
     // Update is called once per frame
     void Update()
     {
-        //agent.SetDestination(curator.GetComponent<Curator>().getArtwork());
+        if(arrived == true)
+        {
+            agent.SetDestination(curator.GetComponent<Curator>().getArtwork());
+            Debug.Log("New Desitination");
+            arrived = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Artwork"))
+        {
+            arrived = true;
+            Debug.Log("I'm here");
+        }
     }
 }
